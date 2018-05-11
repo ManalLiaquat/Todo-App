8c16b558742d4d8956aab0893b916cf2ef940248
ManalLiaquat <m.manalliaquat@gmail.com>
2018-05-11 21:29:57 +0500

    This is a JS file for registration process

---
+++ js/registration.js
@@ -0,0 +1,95 @@
+function register() {
+    var name = document.getElementById("name");
+    var number = document.getElementById("number");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    if (
+        name.value === "" &&
+        email.value === "" &&
+        password.value === "" &&
+        number.value === ""
+    ) {
+        alert("Please fill the form first");
+    }
+
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email.value, password.value)
+        .then(function (result) {
+            var obj = {
+                username: name.value,
+                email: result.email,
+                phoneNumber: number.value,
+                uid: result.uid,
+                emailVerified: result.emailVerified
+            };
+
+
+            firebase
+                .database()
+                .ref("ToDo App")
+                .child("Registration Data/" + result.uid)
+                .set(obj);
+            console.log(result);
+            setTimeout(() => {
+                name.value = "";
+                number.value = "";
+                email.value = "";
+                password.value = "";
+                alert("You are signed in successfully");
+            }, 1000);
+        })
+        .catch(function (error) {
+            console.log(error);
+            if (
+                !(
+                    name.value === "" &&
+                    email.value === "" &&
+                    password.value === "" &&
+                    number.value === ""
+                )
+            ) {
+                alert(error.message)
+            }
+        });
+
+}
+
+function login() {
+    var userEmail = document.getElementById("userEmail");
+    var userPass = document.getElementById("userPassword");
+
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(userEmail.value, userPass.value)
+        .then(function (result) {
+            alert("You're logged in successfully");
+
+            console.log(result.uid);
+            setTimeout(() => {
+                window.location = "app.html";
+            }, 1000);
+        })
+        .catch(function (error) {
+            console.log(error);
+            alert(error.message);
+        });
+}
+
+
+
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        var uid = user.uid;
+        console.log(uid);
+    } else {
+        console.log("Please login to see your todos");
+        console.log("User is signed out.\nPlease login!");
+    }
+
+});
+
+
+/* Registration End */

52f5319c10ffa663c9fe3dcb701ad5fcdf0fa2d2
ManalLiaquat <m.manalliaquat@gmail.com>
2018-05-28 03:04:53 +0500

    I added service worker file to make todo app runs offline and can be installed on smartphones

---
+++ sw.js
@@ -0,0 +1,27 @@
+var cacheName = 'PWA TodoApp';
+var filesToCache = [
+  '/',
+  './index.html',
+  './css/style.css',
+  './app.html',
+  './js/app.js'
+];
+self.addEventListener('install', function (e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
